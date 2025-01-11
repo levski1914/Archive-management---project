ecc2cc1f6015dd83bc05df963d441c153eb33be6
Georgi Dimitrov <austria1233@abv.bg>
2025-01-11 18:14:43 +0200

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Това трябва да е / ако сайтът ти е на коренния адрес, или поддиректория ако е нужно
   css: {
-    postcss: "./postcss.config.js", // Уверете се, че този файл съществува
+    postcss: "./postcss.config.js", // Увери се, че този файл съществува
+  },
+  build: {
+    target: "esnext", // Поставя съвременен target, за да гарантира, че Vite генерира модерен код
   },
 });
